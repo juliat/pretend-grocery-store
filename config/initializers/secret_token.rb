5aa026fc0001a4575be180245151bc82a6626a92
Julia Teitelbaum <julialt@gmail.com>
2013-02-27 02:27:27 -0500

    basic rails app generated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PretendGroceryStore::Application.config.secret_token = '94e0c24c397de0fa408de6b089f04d66b1dcb8f7010c0f6dae16dab911c08e647e7958b9634c7a2ec75429ec2f78196184a154e7aae2851f628cc7b3d284934e'
